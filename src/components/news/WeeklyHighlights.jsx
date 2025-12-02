ba830083dbcfcd536ef658c4ca640c35398b6a5a
caliothemes <caliothemes@gmail.com>
2025-12-02 16:46:16 +0000

    File changes

--- src/components/news/WeeklyHighlights.jsx
+++ src/components/news/WeeklyHighlights.jsx
@@ -45,7 +45,7 @@ export default function WeeklyHighlights({ articles }) {
       </div>
 
       {/* Articles Grid */}
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid md:grid-cols-2 gap-6">
         {highlightArticles.map((article) => (
           <Link
             key={article.id}
