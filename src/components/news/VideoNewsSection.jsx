4563c951ec9caff9840d713d033a55db5c6e13b3
caliothemes <caliothemes@gmail.com>
2025-12-03 17:40:36 +0000

    File changes

--- src/components/news/VideoNewsSection.jsx
+++ src/components/news/VideoNewsSection.jsx
@@ -67,7 +67,7 @@ export default function VideoNewsSection() {
         </div>
 
         {/* Video Grid */}
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
           {videos.map((video) => (
             <div
               key={video.id}
